313ff973adcb00d6125fc3aba9a71793cb6f7de0
ossamaazzaz <o.azzazrahmani@esi-sba.dz>
2019-07-31 16:57:44 +0100

    adding prepared orders page

--- routes/seller.js
+++ routes/seller.js
@@ -5,11 +5,13 @@ const router = express.Router();
 const homeController = require("../controllers/seller/home");
 const addOrderController = require("../controllers/seller/addOrder");
 const dbController = require("../controllers/seller/db");
+const preparedOrdersController = require("../controllers/seller/preparedOrders");
 
 //require middleware
 
 //handling requests 
 router.get("/", dbController);
+router.get("/prepared", preparedOrdersController);
 router.get("/create", homeController);
 router.post("/orders/add", addOrderController);
 
