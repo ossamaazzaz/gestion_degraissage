56cf8cc2e50fb4d612a893ef5e94c17376e3366a
ossamaazzaz <o.azzazrahmani@esi-sba.dz>
2019-07-31 16:37:07 +0100

    adding 'to_prepare' orders page

---
+++ controllers/seller/db.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const Order = require("../../models/Order");
+mongoose.connect("mongodb://localhost/degraissage", { useNewUrlParser: true });
+
+module.exports = async (req, res) => {
+    const orders = await Order.find({ state: "to_prepare" });
+    res.render("seller.dashboard", { orders });
+}
--- routes/seller.js
+++ routes/seller.js
@@ -4,10 +4,13 @@ const router = express.Router();
 //require controllers
 const homeController = require("../controllers/seller/home");
 const addOrderController = require("../controllers/seller/addOrder");
+const dbController = require("../controllers/seller/db");
+
 //require middleware
 
 //handling requests 
-router.get("/", homeController);
+router.get("/", dbController);
+router.get("/create", homeController);
 router.post("/orders/add", addOrderController);
 
 module.exports = router;
