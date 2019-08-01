e5098fa2ee58543ecbc99b4fd513064375b7a18b
ossamaazzaz <o.azzazrahmani@esi-sba.dz>
2019-08-01 12:42:25 +0100

    counting orders money with finance

--- controllers/api/makeRecovered.js
+++ controllers/api/makeRecovered.js
@@ -1,8 +1,20 @@
 const mongoose = require("mongoose");
 const Order = require("../../models/Order");
+const Info = require("../../models/info");
+const Finance = require("../../models/finance");
 mongoose.connect("mongodb://localhost/degraissage", { useNewUrlParser: true });
 
 module.exports = async (req, res) => {
+    let info = await Info.findOne({});
+    let allMoney = parseInt(info.money);
+    let order = await Order.findOne({ _id: req.params.id });
     await Order.updateOne({ _id: req.params.id }, { state: "recovered" });
+    allMoney += parseInt(order.toPay);
+    await Info.updateOne({ _id: info._id }, { money: allMoney });
+    await Finance.create({
+        sign: "+",
+        money: order.toPay,
+        cause: "Client a payé lorsque il a récuperer la commande: " + order.id
+    });
     res.send({ success: true });
 }
--- controllers/seller/addOrder.js
+++ controllers/seller/addOrder.js
@@ -1,10 +1,21 @@
 const mongoose = require("mongoose");
 const Order = require("../../models/Order");
+const Info = require("../../models/info");
+const Finance = require("../../models/finance");
 mongoose.connect("mongodb://localhost/degraissage", { useNewUrlParser: true });
 
 module.exports = async (req, res) => {
     req.body.toPay = parseInt(req.body.price) - parseInt(req.body.paid);
     req.body.state = "to_prepare";
-    await Order.create(req.body);
+    let info = await Info.findOne({});
+    let allMoney = parseInt(info.money);
+    allMoney += parseInt(req.body.paid);
+    await Info.updateOne({ _id: info._id }, { money: allMoney })
+    let order = await Order.create(req.body);
+    await Finance.create({
+        sign: "+",
+        money: req.body.paid,
+        cause: "Client a payé lorsque il pose la commande: " + order.id
+    });
     res.redirect("/");
 }
